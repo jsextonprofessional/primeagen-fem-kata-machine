6d4ad3d0a94206babd20bde64366a87374df83ff
Jacob Sexton <jsextonprofessional@protonmail.com>
2024-08-27 17:21:16 -0400

    Installs dependencies after fork. Solves LinearSearchList and BinarySearchList.

---
+++ src/day2/BinarySearchList.ts
@@ -0,0 +1,20 @@
+export default function bs_list(haystack: number[], needle: number): boolean {
+
+  let low = 0;
+  let high = haystack.length;
+
+  do {
+    const midpoint = Math.floor(low + (high - low) / 2);
+    const value = haystack[midpoint]
+
+    if (value === needle) {
+      return true;
+    } else if (value < needle) {
+      low = midpoint + 1
+    } else high = midpoint
+  } while (low < high)
+
+  return false;
+  // or -1
+
+}

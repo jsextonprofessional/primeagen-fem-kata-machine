c3c882c1a025960914d8ae1a13b714e041681e8c
Jacob Sexton <jsextonprofessional@protonmail.com>
2024-08-28 16:09:45 -0400

    Solves Queue.

--- src/day1/SinglyLinkedList.ts
+++ src/day1/SinglyLinkedList.ts
@@ -1,27 +1,34 @@
+type Node<T> = {
+  value: T,
+  next?: Node<T>
+}
+
 export default class SinglyLinkedList<T> {
-    public length: number;
+  public length: number;
+  private head?: Node<T>;
+  private tail?: Node<T>;
 
-    
+  constructor() {
+    this.head = this.tail = undefined;
+    this.length = 0;
+  }
 
-    constructor() {
-    }
+  prepend(item: T): void {
 
-    prepend(item: T): void {
+  }
+  insertAt(item: T, idx: number): void {
 
-}
-    insertAt(item: T, idx: number): void {
+  }
+  append(item: T): void {
 
-}
-    append(item: T): void {
+  }
+  remove(item: T): T | undefined {
 
-}
-    remove(item: T): T | undefined {
+  }
+  get(idx: number): T | undefined {
 
-}
-    get(idx: number): T | undefined {
+  }
+  removeAt(idx: number): T | undefined {
 
-}
-    removeAt(idx: number): T | undefined {
-
-}
+  }
 }

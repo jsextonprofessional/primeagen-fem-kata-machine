fccf07c587eaff92efbce5338b65ef86b6fc8b8f
Jacob Sexton <jsextonprofessional@protonmail.com>
2024-09-02 13:22:53 -0400

    Adds BTBFS.

--- src/day2/BTBFS.ts
+++ src/day2/BTBFS.ts
@@ -1,3 +1,18 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q: (BinaryNode<number> | null)[] = [head];
 
+  while (q.length) {
+    const current = q.shift() as BinaryNode<number>;
+    if (!current) {
+      continue;
+    }
+    // search
+    if (current.value === needle) {
+      return true;
+    }
+
+    q.push(current.left)
+    q.push(current.right)
+  }
+  return false;
 }
